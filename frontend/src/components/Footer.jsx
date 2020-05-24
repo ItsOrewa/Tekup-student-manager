e537ba1ca193912c56534834399266253c328c02
ItsOrewa <jouiniarwa95@gmail.com>
2020-05-24 18:19:30 +0100

    Edit Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 function Footer(){
     return <footer>
-        <p>Made with Love by AmEr and Orewa</p>
+        <a href="https://www.facebook.com/tekup.de/" class="fa fa-facebook"></a>
+        <a href="mailto: " class="fa fa-google"></a>
+        <a href="https://twitter.com/tek__up?lang=fr" class="fa fa-twitter"></a>
+        <a href="https://fr.linkedin.com/school/tek-up/" class="fa fa-linkedin"></a>
+        <a href="https://www.youtube.com/channel/UCOQH2uLedOdrFbKKbVnwLjA" class="fa fa-youtube"></a>
+        <a href="https://tek-up.de" class="fa fa-rss"></a>
+        <p>Copyright &#169; 2020 Tekup </p>
     </footer>
 }
 
