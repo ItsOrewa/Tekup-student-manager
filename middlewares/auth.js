b47c56ac1748581b398bf98e177360067bb0263b
AmEr-Tinsley <amerhosni07@gmail.com>
2020-08-05 19:49:43 +0100

    auth fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,10 +4,6 @@ process.env.SECRET_KEY = 'secret'
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.cookie.split(' ')[2].split('=')[1];
-    if(token===undefined){
-      res.render('landing_section/index.ejs');
-      return;
-    }
     const decoded = jwt.verify(token,process.env.SECRET_KEY)
     req.user = decoded;
     next();
